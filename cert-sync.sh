1cc01b95b2fe40b6080e24bc639ab3f28d3464cb
Jeremy Felt <jeremy.felt@gmail.com>
2017-09-29 12:37:04 -0700

    Add generation dates and authors to script/templates

--- cert-sync.sh
+++ cert-sync.sh
@@ -4,6 +4,9 @@ dns=$(dig @8.8.8.8 +short "$domain")
 dns=(${dns[@]})
 dns="${dns[0]}"
 
+generated=$(date)
+generator=$(whoami)
+
 if [[ "wsuwp-prod-01.web.wsu.edu." = $dns || "134.121.140.68" = $dns ]]; then
   if [[ ! -z "$2" && "no-www" = "$2" ]]; then
     certbot-auto certonly --webroot -w /var/www/wordpress/ -d $domain
@@ -19,6 +22,9 @@ if [[ "wsuwp-prod-01.web.wsu.edu." = $dns || "134.121.140.68" = $dns ]]; then
   sed -i '' -e "s/DOMAINS/$domain www.$domain/g" nginx-config/$domain.conf
 
   sed -i '' -e "s/DOMAIN/$domain/g" nginx-config/$domain.conf
+
+  sed -i '' -e "s/GENERATED/$generated/g" nginx-config/$domain.conf
+  sed -i '' -e "s/GENERATOR/$generator/g" nginx-config/$domain.conf
 else
   echo "Public DNS records are not ready for certificate authorization."
 fi
