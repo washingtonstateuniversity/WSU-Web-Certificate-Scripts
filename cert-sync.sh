702a6c2e0a00ad2d0400087e92cab2a2c1217abb
Jeremy Felt <jeremy.felt@gmail.com>
2017-09-29 12:24:10 -0700

    Account for CNAME and A records in exploratory `dig`

--- cert-sync.sh
+++ cert-sync.sh
@@ -1,6 +1,10 @@
 domain=$1
 
-if [[ $(dig cname @8.8.8.8 +short "$domain" | grep wsuwp-prod-01 ) ]]; then
+dns=$(dig @8.8.8.8 +short "$domain")
+dns=(${dns[@]})
+dns="${dns[0]}"
+
+if [[ "wsuwp-prod-01.web.wsu.edu." = $dns || "134.121.140.68" = $dns ]]; then
   if [[ ! -z "$2" && "no-www" = "$2" ]]; then
     certbot-auto certonly --webroot -w /var/www/wordpress/ -d $domain
     template="le-cert-no-www.template.conf"
